a01dfe584e715068a1927e339d7250609f12289e
ItsJustWesley <wesleyoonk00@gmail.com>
2018-10-03 09:30:32 +0200

    Inleverformulier zonder opmaak (Rik, looking at you)

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<body>
+
+<h1>Inleveren voor studenten</h1>
+<p>
+    <form action="/index.php">
+     Studiejaar:<br>
+    <input type="radio" name="studiejaar" value="2016-2017"> 2016-2017<br>
+    <input type="radio" name="studiejaar" value="2017-2018"> 2017-2018<br>
+    <input type="radio" name="studiejaar" value="2018-2019"> 2018-2019<br>
+    <br>
+    Studierichting:<br>
+    <input type="radio" name="studierichting" value="business"> Business<br>
+    <input type="radio" name="studierichting" value="software_engineering"> Software Engineering<br>
+    <input type="radio" name="studierichting" value="itsm"> IT Service Management<br>
+    <br>
+    Productnaam:<br>
+    <input type="text" name="productnaam"><br>
+    <br>
+    Bedrijfsnaam:<br>
+    <input type="text" name="bedrijfsnaam"><br>
+    <br>
+    Soort opdracht:<br>
+    <input type="radio" name="soort_opdracht" value="stage" > Stage opdracht <br>
+    <input type="radio" name="soort_opdracht" value="afstudeer" > Afstudeer opdracht <br>
+    <br>
+    Selecteer de opdracht: <input type="file" name="opdracht"><br>
+    <br>
+    Geef een samenvatting van het gemaakte werk.<br>
+    <textarea rows="10" cols="50" name="samenvatting">   </textarea>
+
+</form>
+
+</p>
+
+</body>
+</html>
