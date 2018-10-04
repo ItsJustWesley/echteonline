3cb32edf4b6c86b4ac9ddcbfa8212a4d3c059e86
ItsJustWesley <wesleyoonk00@gmail.com>
2018-10-04 12:08:24 +0200

    Inleverpagina met opmaak (concept)

--- index.php
+++ index.php
@@ -1,36 +1,48 @@
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/html">
+<html xmlns="http://www.w3.org/1999/html"Studentenportaal
+<head>
+    <title>Studentenportaal</title>
+    <link rel="stylesheet" media="all" href="/public.css" />
+</head>
 <body>
+
+<?php include ('C:\Users\wesle\Documents\School\Kwartiel 2.1\Project Online\Online\globe_bank\public_header.php'); ?>
+
 
 <h1>Inleveren voor studenten</h1>
 <p>
+    <div>
     <form action="/index.php">
      Studiejaar:<br>
-    <input type="radio" name="studiejaar" value="2016-2017"> 2016-2017<br>
-    <input type="radio" name="studiejaar" value="2017-2018"> 2017-2018<br>
-    <input type="radio" name="studiejaar" value="2018-2019"> 2018-2019<br>
+    <input type="radio" name="studiejaar" value="2016-2017" required> 2016-2017<br>
+    <input type="radio" name="studiejaar" value="2017-2018" required> 2017-2018<br>
+    <input type="radio" name="studiejaar" value="2018-2019" required> 2018-2019<br>
     <br>
     Studierichting:<br>
-    <input type="radio" name="studierichting" value="business"> Business<br>
-    <input type="radio" name="studierichting" value="software_engineering"> Software Engineering<br>
-    <input type="radio" name="studierichting" value="itsm"> IT Service Management<br>
+    <input type="radio" name="studierichting" value="business" required> Business<br>
+    <input type="radio" name="studierichting" value="software_engineering" required> Software Engineering<br>
+    <input type="radio" name="studierichting" value="itsm" required> IT Service Management<br>
     <br>
     Productnaam:<br>
-    <input type="text" name="productnaam"><br>
+    <input type="text" name="productnaam" required><br>
     <br>
     Bedrijfsnaam:<br>
-    <input type="text" name="bedrijfsnaam"><br>
+    <input type="text" name="bedrijfsnaam" required><br>
     <br>
     Soort opdracht:<br>
-    <input type="radio" name="soort_opdracht" value="stage" > Stage opdracht <br>
-    <input type="radio" name="soort_opdracht" value="afstudeer" > Afstudeer opdracht <br>
+    <input type="radio" name="soort_opdracht" value="stage" required> Stage opdracht <br>
+    <input type="radio" name="soort_opdracht" value="afstudeer" required> Afstudeer opdracht <br>
     <br>
-    Selecteer de opdracht: <input type="file" name="opdracht"><br>
+    Selecteer de opdracht: <input type="file" name="opdracht" required><br>
     <br>
-    Geef een samenvatting van het gemaakte werk.<br>
-    <textarea rows="10" cols="50" name="samenvatting">   </textarea>
+    Geef een samenvatting van het gemaakte werk en het bedrijf waar je de opdracht hebt gemaakt.<br>
+    <textarea rows="10" cols="50" name="samenvatting" required> </textarea> <br>
+    <br>
+        <input type="submit" value="Inleveren"
 
 </form>
+</div>
+
 
 </p>
 
---
+++ public_header.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" media="all" href="/public.css" />
+  </head>
+ <style>
+  div.login {
+  position: relative;
+  left: 800px;
+  bottom: 115px;
+  border: none;
+  }
+  </style>
+  <body>
+
+    <header>
+      <h1>
+          <img src="/pictures/SaxionLogo.png" width="200" height="80" alt="" />
+      </h1>
+        <div class="login">
+            Gebruikersnaam:<br>
+            <input type="text" name "gebruikersnaam"> <br>
+            Wachtwoord: <br>
+            <input type="password" name="password"> <br>
+            <input type="button" onclick="alert('Hoppate')" value="Inloggen">
+
+        </div>
+    </header>
+
+
+
